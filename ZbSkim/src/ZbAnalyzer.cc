24b32c5e6002c4483d89af15f7d8911dc956ff96
vieri <>
2013-03-06 10:15:42 +0000

    version 2.0

--- ZbSkim/src/ZbAnalyzer.cc
+++ ZbSkim/src/ZbAnalyzer.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Vieri Candelise
 //         Created:  Thu Jan 10 15:57:03 CET 2013
-// $Id$
+// $Id: ZbAnalyzer.cc,v 1.1 2013/01/28 12:52:11 dellaric Exp $
 //
 //
 
@@ -36,6 +36,7 @@
 #include <sstream>
 #include <cmath>
 #include <stddef.h>
+#include <string>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -97,6 +98,9 @@ class ZbAnalyzer : public edm::EDAnalyzer {
 
 
    private:
+
+
+
       virtual void beginJob() ;
       virtual void analyze(const edm::Event&, const edm::EventSetup&);
       virtual void endJob() ;
@@ -106,15 +110,24 @@ class ZbAnalyzer : public edm::EDAnalyzer {
       virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
       virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
 
+      std::string pileup_;
+      
+
       // ----------member data ---------------------------
    
-      int Nj;
+      int Nj, Nb;
       double jet_pt;
+      double jet_phi;
       double ele_pt;
       double muon_pt;
       double diele_inv;
       double dimuon_inv;
-
+      double diele_phi;
+      double dimuon_phi;
+      double Delta_phi_mm;
+      double b_leading_pt;
+      double  b_mm_invmass;
+      double  b_ee_invmass;
       int NZ, NZ2;
       double discrCSV;
       double MyWeight;
@@ -129,15 +142,47 @@ class ZbAnalyzer : public edm::EDAnalyzer {
       TH1F* h_ee_inv; 
       TH1F* h_secondvtx_N;
       TH1F* h_PUweights;
+      TH1F* h_tracks;
       TH1D* recoVTX_;
       TH1D* recoVTX_w;
+      
+      TH1F* w_jetmultiplicity;
+      TH1F* w_bmultiplicity;
+      TH1F* w_jet_pt;
+      TH1F* w_ele_pt;
+      TH1F* w_muon_pt;
+      TH1F* w_mm_inv;
+      TH1F* w_ee_inv; 
+      TH1F* w_secondvtx_N;
+      TH1F* w_bquarks;
+      TH1F* w_tracks;
+      TH1F* flavours_;
+      TH1F* w_MET;
+      TH1F* w_delta_phi_mm;
 
+      TH1F* b_mm_inv;
+      TH1F* b_ee_inv;
+      TH1F* w_bleading_pt;
 };
 	using namespace pat;
 
 //
 // constants, enums and typedefs
 //
+      enum Flavour {
+		ALL_JETS = 0,
+		UDSG_JETS,
+		C_JETS,
+		B_JETS,
+		NONID_JETS,
+		N_JET_TYPES
+	};
+		struct Plots {
+			TH1 *nVertices;
+			TH1 *deltaR, *mass, *dist, *distErr, *distSig;
+			TH1 *nTracks, *chi2;
+		} plots_[N_JET_TYPES];
+
 
 //
 // static data member definitions
@@ -149,19 +194,41 @@ class ZbAnalyzer : public edm::EDAnalyzer {
 ZbAnalyzer::ZbAnalyzer(const edm::ParameterSet& iConfig)
 {
 
+   pileup_ = iConfig.getUntrackedParameter<std::string>("pileup", "S7");
+
    //now do what ever initialization is needed
    edm::Service<TFileService> fs; 
 
-   h_jetmultiplicity = fs->make<TH1F>("h_jetmultiplicity","h_jetmultiplicity",6,0,6);
-   h_jet_pt = fs->make<TH1F>("h_jet_pt","h_jet_pt",100,0,100); 
-   h_ele_pt = fs->make<TH1F>("h_ele_pt","h_ele_pt",100,0,100); 
-   h_muon_pt = fs->make<TH1F>("h_muon_pt","h_muon_pt",100,0,100); 
+   h_jetmultiplicity = fs->make<TH1F>("h_jetmultiplicity","h_jetmultiplicity",8,0.5,8.5);
+   h_jet_pt = fs->make<TH1F>("h_jet_pt","h_jet_pt",20,30,530); 
+   h_ele_pt = fs->make<TH1F>("h_ele_pt","h_ele_pt",20,30,530); 
+   h_muon_pt = fs->make<TH1F>("h_muon_pt","h_muon_pt",100,0,250); 
    h_mm_inv = fs->make<TH1F>("h_mm_inv","h_mm_inv",60,60,120); 
    h_ee_inv = fs->make<TH1F>("h_ee_inv","h_ee_inv",60,60,120); 
-   h_secondvtx_N = fs->make<TH1F>("h_secondvtx_N","h_secondvtx_N",10,0,2); 
+   h_secondvtx_N = fs->make<TH1F>("h_secondvtx_N","h_secondvtx_N",50,0,1); 
    h_PUweights   = fs->make<TH1F>("h_pu_weights", "h_pu_weights", 10, 0, 10);
    recoVTX_ =        fs->make<TH1D>("recoVTX","No. reconstructed vertices",40,0.,40.);
    recoVTX_w =        fs->make<TH1D>("recoVTXw","No. reconstructed vertices weighted",40,0.,40.);
+   h_tracks  = fs->make<TH1F>("h_tracks","h_tracks",50,0,50);
+    
+   //weighted histograms
+   w_jetmultiplicity = fs->make<TH1F>("w_jetmultiplicity","w_jetmultiplicity",8,0.5,8.5);
+   w_bmultiplicity = fs->make<TH1F>("w_bjetmultiplicity","w_bjetmultiplicity",5,0.5,5.5);
+   w_jet_pt = fs->make<TH1F>("w_jet_pt","w_jet_pt", 20,30,530);
+   w_bleading_pt = fs->make<TH1F>("w_bleading_pt","w_bleading_pt",20,30,530);
+   w_ele_pt = fs->make<TH1F>("w_ele_pt","w_ele_pt",100,0,250); 
+   w_muon_pt = fs->make<TH1F>("w_muon_pt","w_muon_pt",100,0,250); 
+   w_mm_inv = fs->make<TH1F>("w_mm_inv","w_mm_inv",60,60,120); 
+   w_ee_inv = fs->make<TH1F>("w_ee_inv","w_ee_inv",60,60,120); 
+   w_secondvtx_N = fs->make<TH1F>("w_secondvtx_N","w_secondvtx_N",50,0,1); 
+   w_bquarks = fs->make<TH1F>("bquarks","bquarks",50,0,1);
+   w_tracks  = fs->make<TH1F>("w_tracks","w_tracks",50,0,50);
+   flavours_ = fs->make<TH1F>("flavours", "jet flavours", 5, 0, 5);
+   w_MET  = fs->make<TH1F>("w_MET","w_MET",100,0,250);
+   w_delta_phi_mm  = fs->make<TH1F>("w_delta_phi_mm","w_delta_phi_mm", 12,0,TMath::Pi());
+
+   b_mm_inv = fs->make<TH1F>("b_mm_inv","b_mm_inv",60,60,120); 
+   b_ee_inv = fs->make<TH1F>("b_ee_inv","b_ee_inv",60,60,120); 
 
    treeZb_ = fs->make<TTree>("ZbTree","ZbTree");
    treeZb_ ->Branch("Nj", &Nj);
@@ -171,6 +238,38 @@ ZbAnalyzer::ZbAnalyzer(const edm::ParameterSet& iConfig)
    treeZb_ ->Branch("diele_inv", &diele_inv);
    treeZb_ ->Branch("weights_pu", &MyWeight);
 
+   for(unsigned int i = 0; i < N_JET_TYPES; i++) {
+	   Plots &plots = plots_[i];
+	   const char *flavour, *name;
+
+
+	   switch((Flavour)i) {
+		   case ALL_JETS:
+			   flavour = "all jets";
+			   name = "all";
+			   break;
+		   case UDSG_JETS:
+			   flavour = "light flavour jets";
+			   name = "udsg";
+			   break;
+		   case C_JETS:
+			   flavour = "charm jets";
+			   name = "c";
+			   break;
+		   case B_JETS:
+			   flavour = "bottom jets";
+			   name = "b";
+			   break;
+		   default:			  
+			   flavour = "unidentified jets";			
+			   name = "ni";						  
+			   break;
+	   }
+	   plots.dist = fs->make<TH1F>(Form("dist_%s", name),
+			   Form("Transverse distance between PV and SV in %s", flavour), 100, 0, 2);
+
+
+}
 }
 
 ZbAnalyzer::~ZbAnalyzer()
@@ -202,6 +301,7 @@ ZbAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 	   std::vector<PileupSummaryInfo>::const_iterator PVI;
 
 	   float Tnpv = -1;
+	   
 	   for(PVI = PupInfo->begin(); PVI != PupInfo->end(); ++PVI) {
 		   int BX = PVI->getBunchCrossing();
 		   if(BX == 0) { 
@@ -211,13 +311,14 @@ ZbAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 	   }
 
 	   edm::LumiReWeighting LumiWeights_;
-	   LumiWeights_ = edm::LumiReWeighting("/gpfs/cms/users/candelis/work/Zb/pileup/pileup_S7.root", 
+	   LumiWeights_ = edm::LumiReWeighting("/gpfs/cms/users/candelis/work/Zb/pileup/pileup_" + pileup_ + ".root", 
 			   		       "/gpfs/cms/users/candelis/work/Zb/pileup/pileup_2012.root", "pileup", "pileup");
    
 	   MyWeight = LumiWeights_.weight( Tnpv );
+
    }
 
-   cout<<"weight="<<MyWeight<<endl;
+   //cout<<"weight="<<MyWeight<<endl;
 
    //get vertices 
     edm::Handle< std::vector<reco::Vertex> > vertices_h;
@@ -248,14 +349,6 @@ ZbAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
      //recoVTX_->Fill(float(NVtx));
      //recoVTX_w ->Fill(NVtx, MyWeight);
 
-   //get electron collection
-   //edm::Handle<std::vector<reco::PFCandidate> > electrons;
-   //iEvent.getByLabel("pfIsolatedElectronsPFlow", electrons);
-
-   //get muon collection
-   //edm::Handle<std::vector<reco::PFCandidate>  > muons;
-   //iEvent.getByLabel("pfIsolatedMuonsPFlow", muons);
-   
    //get muon collection
    edm::Handle<pat::MuonCollection> Trigmuons;
    iEvent.getByLabel("matchedMuons", Trigmuons);
@@ -263,10 +356,6 @@ ZbAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    //get muon collection
    edm::Handle<pat::ElectronCollection> Trigelectrons;
    iEvent.getByLabel("matchedElectrons", Trigelectrons);
-
-   //get jet collection
-   //edm::Handle<std::vector<reco::PFJet>  > jets;
-   //iEvent.getByLabel("ak5PFJets",jets);
 
    //get jet collection
    edm::Handle<std::vector<pat::Jet>  > jets;
@@ -279,9 +368,21 @@ ZbAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    // Get the Z->ee collection
    edm::Handle<reco::CompositeCandidateCollection> zee;
    iEvent.getByLabel("zeleMatchedeleMatched", zee);
-  
+
+   // Get tracks
+   edm::Handle<std::vector<reco::Track> >  tracks;
+   iEvent.getByLabel("generalTracks",tracks);
+    
+   //Get MET
+   edm::Handle< std::vector<pat::MET> > met;
+   iEvent.getByLabel(edm::InputTag ("patMETsPFlow"), met);
+
+   int Ntracks=0;
+   int Nmu=0;
    Nj=0;
+   Nb=0;
    jet_pt=0;
+   jet_phi=0;
    ele_pt=0;
    muon_pt=0;
    diele_inv=0;
@@ -289,97 +390,189 @@ ZbAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    NZ=0;
    NZ2=0;
    discrCSV=0; 
-
-    //test on muon Vs muonmatched///////////////////////////////////
-
-	           //cout<<"mu size="<<muons->size()<<endl;
-	           cout<<"Trig ele size="<<Trigelectrons->size()<<endl;
-	           cout<<"Trig muo size="<<Trigmuons->size()<<endl;
-  	           //cout<<"Z="<<Zbosons->size()<<endl; 
-   		   //cout<<"Z="<<zmm->size()<<endl;
-
-   ///////////////////////////////////////////////////////
+   b_leading_pt=0;
+   Delta_phi_mm=0;
+   b_mm_invmass=0;
+   b_ee_invmass=0;
+   
+if(zmm->size()!=0 || zee->size()!=0){
    
    // Loop over pat jets
    for(std::vector<pat::Jet>::const_iterator jet=jets->begin(); jet!=jets->end(); ++jet){
-	    if(jet->pt() > 30 && abs(jet->eta()) < 2.5
+	    if(jet->pt() > 30 && abs(jet->eta()) < 2.4
 	       && jet->chargedEmEnergyFraction()<0.99
 	       && jet->neutralHadronEnergyFraction()<0.99
 	       && jet->neutralEmEnergyFraction()<0.99
 	       && jet->chargedHadronEnergyFraction()>0
-	       && jet->chargedMultiplicity()>0){	
-	
-		++Nj;
+	       && jet->chargedMultiplicity()>0){		
+	       
+	        ++Nj;
 	        jet_pt = jet->pt();
+
+		if(Nj>0){
 	        h_jet_pt -> Fill(jet_pt);
+	        w_jet_pt -> Fill(jet_pt, MyWeight);
+		}
 
-/*
-const std::vector< std::pair< std::string, float > > discrPairs = jet->getPairDiscri();
-for(uint pair_i = 0;pair_i <discrPairs.size(); ++pair_i ){
-cout << "discr name: "<< discrPairs[pair_i].first<<" value: "<< discrPairs[pair_i].second<<endl;
-      }
-*/
+		/*
+  // Flavour studies 
 
+		Flavour flavour;
+		// find out the jet flavour (differs between quark and anti-quark)
+		switch(std::abs(jet->partonFlavour())) {
+			case 1:
+			case 2:
+			case 3:
+			case 21:
+				flavour = UDSG_JETS;
+				break;
+			case 4:
+				flavour = C_JETS;
+				break;
+			case 5:
+				flavour = B_JETS;
+				break;
+			default:
+				flavour = NONID_JETS;
+		}
+
+			
+		const reco::SecondaryVertexTagInfo &svTagInfo = *jet->tagInfoSecondaryVertex();
+		if (svTagInfo.nVertices() < 1) continue;
+	        const reco::Vertex &sv = svTagInfo.secondaryVertex(0); 
+	
+		// simply count the number of accepted jets
+		flavours_->Fill(ALL_JETS, MyWeight);
+		flavours_->Fill(flavour, MyWeight);
+
+		// dxy
+		Measurement1D distance = svTagInfo.flightDistance(0, true);
+		plots_[flavour].dist->Fill(distance.value());
+ 
+		*/
+                
+   // b studies
+	
                 discrCSV = jet->bDiscriminator("combinedSecondaryVertexBJetTags");
-		cout<<"CSV discrim = "<< discrCSV <<endl;
-		
-		//const reco::SecondaryVertexTagInfo &svTagInfo = *jet->tagInfoSecondaryVertex();
-		//if (svTagInfo.nVertices() < 1) continue;
-	        //const reco::Vertex &sv = svTagInfo.secondaryVertex(0);
-
-                 
-    	    }
+                	
+                if(abs(jet->partonFlavour())==5 && Nj>0){
+                	w_bquarks->Fill(discrCSV, MyWeight);
+                }
+                
+                w_secondvtx_N ->Fill(discrCSV, MyWeight);
+                if(discrCSV>0.89) ++Nb;
+                if(discrCSV>0.89) jet_phi = jet->phi();
+		if(discrCSV>0.89 && Nb!=0) b_leading_pt = jet->pt();
+		cout<<"bjetpt="<<b_leading_pt<<endl;
+	    }
    } 
+   
+   // MET
+
+     for(std::vector<pat::MET>::const_iterator m=met->begin(); m!=met->end(); ++m){
+	   w_MET->Fill(m->et());
+     }
+
+     for(std::vector<reco::Track>::const_iterator t=tracks->begin(); t!=tracks->end(); ++t){
+ 	   Ntracks = t->numberOfValidHits();
+     }
 
    // Loop over triggered matched muons
 
-   for(pat::MuonCollection::const_iterator mu=Trigmuons->begin(); mu!=Trigmuons->end(); ++mu){
+     for(pat::MuonCollection::const_iterator mu=Trigmuons->begin(); mu!=Trigmuons->end(); ++mu){
 	   if(mu->pt()>25 && abs(mu->eta())<2.5){
+	      Nmu++;
 	      muon_pt = mu->pt();
-              if(muon_pt!=0) h_muon_pt->Fill(muon_pt);
+              if(muon_pt!=0 && Nj>0) {
+		      h_muon_pt->Fill(muon_pt);
+		      w_muon_pt->Fill(muon_pt, MyWeight);
+		      cout<<"mu N="<<Nmu<<"  "<<"pt="<<muon_pt<<endl;
+	      }
+      		      	      
 	   }
-   }
+     }
 
    // Loop over triggered matched muons
 
-   for(pat::ElectronCollection::const_iterator el=Trigelectrons->begin(); el!=Trigelectrons->end(); ++el){
+     for(pat::ElectronCollection::const_iterator el=Trigelectrons->begin(); el!=Trigelectrons->end(); ++el){
            if(el->pt()>25 && abs(el->eta())<2.5){
               ele_pt = el->pt();
-	      if(ele_pt!=0) h_ele_pt->Fill(ele_pt);
+	      if(ele_pt!=0 && Nj>0){
+		      h_ele_pt->Fill(ele_pt);
+		      w_ele_pt->Fill(ele_pt, MyWeight);
+	      }
            }
-   }
+     }
 
    // Loop over Z bosons candidates
 
       for (std::vector<reco::CompositeCandidate>::const_iterator Zit = zmm->begin() ; Zit != zmm->end(); ++Zit){
 
 	     NZ++;
-  	     cout<<"mass mm="<<Zit->mass()<<endl;
+  	     //cout<<"mass mm="<<Zit->mass()<<endl;
 	     dimuon_inv = Zit->mass();
-	     if(dimuon_inv!=0) h_mm_inv->Fill(dimuon_inv);
+	     dimuon_phi = Zit->phi();
+
+	     if(dimuon_inv!=0 && Nj>0){
+		     h_mm_inv->Fill(dimuon_inv);
+		     w_mm_inv->Fill(dimuon_inv, MyWeight);
+		     if(Nb!=0 && discrCSV>0.89) b_mm_invmass = Zit->mass();
+		     b_mm_inv->Fill(b_mm_invmass, MyWeight);
+		     cout<<"bZmass="<<b_mm_invmass<<endl;
+		     
+	     }
       }
       for (std::vector<reco::CompositeCandidate>::const_iterator Zit2 = zee->begin() ; Zit2 != zee->end(); ++Zit2){
 
 	     NZ2++;
   	     cout<<"mass ee="<<Zit2->mass()<<endl;
+
 	     diele_inv = Zit2->mass();
-	     if(diele_inv!=0) h_ee_inv->Fill(diele_inv);
+	     diele_phi = Zit2->phi();
+
+	     if(diele_inv!=0 && Nj>0){
+		     h_ee_inv->Fill(diele_inv);
+		     w_ee_inv->Fill(diele_inv, MyWeight);
+		     if(Nb!=0 && discrCSV>0.89) b_ee_invmass = Zit2->mass();
+		     b_ee_inv->Fill(b_ee_invmass, MyWeight);
+		     cout<<"bZeeinv="<<b_ee_invmass<<endl;
+
+	     }
       }
-
-
-
-   // Fill the tree
 
 	//std::cout<<Nj<<std::endl;
 	treeZb_->Fill();
 
 	h_PUweights ->Fill(MyWeight);
 
-	h_jetmultiplicity -> Fill(Nj);
+	if(Nj>0){
+		h_jetmultiplicity -> Fill(Nj);
+		w_jetmultiplicity -> Fill(Nj, MyWeight);
+	}
+
         h_secondvtx_N ->Fill(discrCSV);
+        //w_secondvtx_N ->Fill(discrCSV, MyWeight);
+
 	recoVTX_w->Fill(float(NVtx)-1, MyWeight);
 	recoVTX_->Fill(float(NVtx)-1);
+	h_tracks->Fill(Ntracks);
+	w_tracks->Fill(Ntracks, MyWeight);   
 
+
+	// Phi studies
+
+	if(jet_phi>0){
+	       	Delta_phi_mm = fabs(dimuon_phi - jet_phi);
+	}
+
+	if((Delta_phi_mm) > acos(-1)) Delta_phi_mm = 2*acos(-1) - Delta_phi_mm;
+
+	w_delta_phi_mm -> Fill(Delta_phi_mm, MyWeight);
+	if(Nb>0) w_bmultiplicity -> Fill(Nb, MyWeight);
+        w_bleading_pt-> Fill(b_leading_pt, MyWeight);
+
+
+   }
 }
 
 // ------------ method called once each job just before starting event loop  ------------
