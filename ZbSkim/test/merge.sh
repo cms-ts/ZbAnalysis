c5642bd8cc578fae3d59152edef0c49f5a9f1ee0
dellaric <>
2013-05-01 21:10:30 +0000

    speed-up merge (don't merge trees)

--- ZbSkim/test/merge.sh
+++ ZbSkim/test/merge.sh
@@ -27,14 +27,14 @@ cd $WORKDIR
 if [ -d $WORKDIR/$VERSION/$JOBDIR ]; then
   echo 'Preparing '$WORKDIR/$VERSION/$JOBDIR.root
   rm -f $WORKDIR/$VERSION/$JOBDIR.root
-  hadd -v 0 $WORKDIR/$VERSION/$JOBDIR.root $WORKDIR/$VERSION/$JOBDIR/LSFJOB_*/rootTuple_*.root
+  hadd -T -v 0 $WORKDIR/$VERSION/$JOBDIR.root $WORKDIR/$VERSION/$JOBDIR/LSFJOB_*/rootTuple_*.root
 elif [ "$JOBDIR" == "data-all" ]; then
   echo 'Preparing '$WORKDIR/$VERSION/DoubleElectron_2012_merge.root
   rm -f $WORKDIR/$VERSION/DoubleElectron_2012_merge.root
-  hadd -v 0 $WORKDIR/$VERSION/DoubleElectron_2012_merge.root $WORKDIR/$VERSION/DoubleElectron_2012*.root
+  hadd -T -v 0 $WORKDIR/$VERSION/DoubleElectron_2012_merge.root $WORKDIR/$VERSION/DoubleElectron_2012*.root
   echo 'Preparing '$WORKDIR/$VERSION/DoubleMu_2012_merge.root
   rm -f $WORKDIR/$VERSION/DoubleMu_2012_merge.root
-  hadd -v 0 $WORKDIR/$VERSION/DoubleMu_2012_merge.root $WORKDIR/$VERSION/DoubleMu_2012*.root
+  hadd -T -v 0 $WORKDIR/$VERSION/DoubleMu_2012_merge.root $WORKDIR/$VERSION/DoubleMu_2012*.root
 else
   echo 'ERROR: jobdir "'$JOBDIR'" does not exist !'
 fi
