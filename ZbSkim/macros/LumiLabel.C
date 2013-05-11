048d7ebfa2a2bb4fa408c0aae93e370fbc24d082
dellaric <>
2013-05-11 07:37:02 +0000

    clean & refine

--- ZbSkim/macros/LumiLabel.C
+++ ZbSkim/macros/LumiLabel.C
@@ -1,29 +1,20 @@
-TLatex *
-CMSPrel (Float_t Lumi, TString _decaychannel)
-{
+TLatex * CMSPrel (Float_t Lumi, TString _decaychannel) {
 
-  TLatex *latexLabel = new TLatex ();
+  TLatex * latexLabel = new TLatex ();
   latexLabel->SetTextSize (0.0275);
   latexLabel->SetTextFont (42);
   latexLabel->SetLineWidth (2);
   latexLabel->SetNDC ();
 
   latexLabel->DrawLatex (0.25, 0.35, "CMS Preliminary");
-  latexLabel->DrawLatex (0.25, 0.30,
-			 "#sqrt{s} = 8 TeV  #int Ldt = 19.6 fb^{-1}");
+  latexLabel->DrawLatex (0.25, 0.30, Form("#sqrt{s} = 8 TeV  #int Ldt = %.1f fb^{-1}", Lumi));
   latexLabel->DrawLatex (0.25, 0.25, "anti-k_{T} (R = 0.5) PF Jets > 30 GeV");
-  //latexLabel->DrawLatex(0.25,0.18,(TString)Form("#int Ldt = %.3f fb^{-1}",Lumi));  
-  //latexLabel->DrawLatex(0.25,0.18,"#int Ldt = 4.890 fb^{-1}");  
-  //latexLabel->DrawLatex(0.25,0.13,"Z#rightarrow ee channel");
   latexLabel->DrawLatex (0.25, 0.08, _decaychannel);
 
-
-  //return latexLabel;
+  return latexLabel;
 }
 
-TLatex *
-CMSPrel (Float_t Lumi, TString _decaychannel, double x, double y)
-{
+TLatex * CMSPrel (Float_t Lumi, TString _decaychannel, double x, double y) {
 
   TLatex *latexLabel = new TLatex ();
   latexLabel->SetTextSize (0.0275);
@@ -31,15 +22,8 @@ CMSPrel (Float_t Lumi, TString _decaychannel, double x, double y)
   latexLabel->SetLineWidth (2);
   latexLabel->SetNDC ();
 
-  latexLabel->DrawLatex (x, y, "CMS Preliminary            #sqrt{s} = 8 TeV #int Ldt = 19.6 fb^{-1}");
-  //latexLabel->DrawLatex (x, y - 0.04,
-  //			 "#sqrt{s} = 8 TeV #int Ldt = 19.6 fb^{-1}");
-  //latexLabel->DrawLatex (x, y - 0.08,
-  //			 "anti-k_{T} (R = 0.5) PF Jets > 30 GeV ");
-  //latexLabel->DrawLatex(x,0.18,(TString)Form("#int Ldt = %.3f fb^{-1}",Lumi));
-  //latexLabel->DrawLatex(x,y-0.18,"Z#rightarrow ee channel");
+  latexLabel->DrawLatex (x, y, Form("CMS Preliminary            #sqrt{s} = 8 TeV #int Ldt = %.1f fb^{-1}", Lumi));
   latexLabel->DrawLatex (x, y - 0.12, _decaychannel);
 
-
-  //return latexLabel;
+  return latexLabel;
 }
