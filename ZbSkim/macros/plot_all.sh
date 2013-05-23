6c4bb5cccc2dfd09b3485f3c253be3b5ba9af485
dellaric <>
2013-05-23 10:13:41 +0000

    add MET_sign

--- ZbSkim/macros/plot_all.sh
+++ ZbSkim/macros/plot_all.sh
@@ -87,6 +87,7 @@ i=1
 while [ $i -le 2 ]; do
 
   root -l -q -b DataMCComp.C\(\"w_MET\",1,$i,0,1\)
+  root -l -q -b DataMCComp.C\(\"w_MET_sign\",1,$i,0,1\)
 
   root -l -q -b DataMCComp.C\(\"w_SVTX_mass\",1,$i,1,0\)
   root -l -q -b DataMCComp.C\(\"w_SVTX_mass\",1,$i,0,2\)
