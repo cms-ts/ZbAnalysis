8885a7d4b6a436303d64cfa522d3e79759dd51d8
dellaric <>
2013-06-20 07:50:04 +0000

    fix typo

--- ZbSkim/test/triggerZbConf_mc_gen_cfg.py
+++ ZbSkim/test/triggerZbConf_mc_gen_cfg.py
@@ -265,7 +265,7 @@ process.dump = cms.EDAnalyzer("EventContentAnalyzer")
 process.MyProcess = cms.EDFilter('ZbFilter')
 process.demo3_ee = cms.EDAnalyzer('GenbAnalyzer',
 			pileup  = cms.untracked.string("S10"),
-				lepton  = cms.untracked.string("electrons"),
+				lepton  = cms.untracked.string("electron"),
 		)
 process.demo3_mm = cms.EDAnalyzer('GenbAnalyzer',
 			pileup  = cms.untracked.string("S10"),
