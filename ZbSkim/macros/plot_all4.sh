ef172ff4416943138a7f1dad78e2ac031dad2e0a
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2013-07-29 16:25:34 +0200

    add new plots about unfolding

---
+++ ZbSkim/macros/plot_all4.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+cd $CMS_PATH/slc5_amd64_gcc472/cms/cmssw/CMSSW_6_1_0
+eval `scramv1 runtime -sh`
+cd -
+
+i=1
+while [ $i -le 2 ]; do
+
+  root -l -q -b DataMCComp4.C\(\"w_first_jet_pt\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_first_jet_eta\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_first_jet_pt_b\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_first_jet_eta_b\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_first_bjet_pt\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_first_bjet_eta\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_pt_Z_ee\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_pt_Z_mm\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_pt_Z_ee_b\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_pt_Z_mm_b\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_Ht\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_Ht_b\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_delta_phi_mm\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_delta_phi_ee\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_delta_phi_mm_b\",1,$i\)
+  root -l -q -b DataMCComp4.C\(\"w_delta_phi_ee_b\",1,$i\)
+
+  i=$((i+1))
+done
+
+exit
