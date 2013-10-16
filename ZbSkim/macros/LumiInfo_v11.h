ba26f3483560187349e709721238f37129071a26
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2013-10-16 15:43:54 +0200

    add theo. xsec errors

--- ZbSkim/macros/LumiInfo_v11.h
+++ ZbSkim/macros/LumiInfo_v11.h
@@ -17,48 +17,57 @@ double Lumi_2012D_22Jan = 7318.000;
 
 double Ngen_dy = 30459503;
 double Xsec_dy = 3503.71; // NNLO
+double eXsec_dy = 0;
 
 //////////////////////// DY Sherpa
 
 double Ngen_dy_1 = 127014144 * 43915740.0 / 127014144.0;
 double Xsec_dy_1 = 3503.71; // NNLO
+double eXsec_dy_1 = 0;
 
 //////////////////////// DY Powheg
 
 double Ngen_dy_2_ee = 3297045; // EE
 double Ngen_dy_2_mm = 3283740; // MM
 double Xsec_dy_2 = 5745.25/3.0; // NNLO
+double eXsec_dy_2 = 0;
 
 //////////////////////// TTbar
 
 double Ngen_tt = 6923750;
 double Xsec_tt = 225.197; // NLO
+double eXsec_tt = 0.07*Xsec_tt;
 
 //////////////////////// ZZ
 
 double Ngen_zz = 9799908;
 double Xsec_zz = 8.059; // NLO with CTEQ
+double eXsec_zz = 0.15*Xsec_zz; 
 
 //////////////////////// WZ
 
 double Ngen_wz = 10000283;
 double Ngen_wz = 9970342;
 double Xsec_wz = 33.21;    // NLO with CTEQ
+double eXsec_wz = 0.15*Xsec_wz; 
 
 //////////////////////// QCD
 
 double Ngen_qcd = 7529312;
 double Xsec_qcd = 3.64E8; // search the NLO !
+double eXsec_qcd = 0.15*Xsec_qcd; 
 
 //////////////////////// WW
 
 double Ngen_ww = 10000431;
 double Xsec_ww = 54.838; // NLO with CTEQ
+double eXsec_ww = 0.15*Xsec_ww; 
 
 //////////////////////// Wj
 
 double Ngen_wj = 57709905;
 double Xsec_wj = 31200; // NLO with CTEQ
+double eXsec_wj = 0.15*Xsec_wj; 
 
 ///////////////////////
 
