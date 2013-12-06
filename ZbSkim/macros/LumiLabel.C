2b7c6e844418a25114d607a3486b468d9283ae2c
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2013-12-06 17:40:59 +0100

    remove "CMS Preliminary"

--- ZbSkim/macros/LumiLabel.C
+++ ZbSkim/macros/LumiLabel.C
@@ -8,7 +8,7 @@ TLatex * CMSPrel (Float_t Lumi, TString _decaychannel) {
   latexLabel->SetLineWidth (2);
   latexLabel->SetNDC ();
 
-  latexLabel->DrawLatex (0.25, 0.35, "CMS Preliminary");
+//  latexLabel->DrawLatex (0.25, 0.35, "CMS Preliminary");
   latexLabel->DrawLatex (0.25, 0.30, Form("#sqrt{s} = 8 TeV  #int Ldt = %.1f fb^{-1}", Lumi));
   latexLabel->DrawLatex (0.25, 0.25, "anti-k_{T} (R = 0.5) PF Jets > 30 GeV");
   latexLabel->DrawLatex (0.25, 0.08, _decaychannel);
@@ -24,7 +24,8 @@ TLatex * CMSPrel (Float_t Lumi, TString _decaychannel, double x, double y) {
   latexLabel->SetLineWidth (2);
   latexLabel->SetNDC ();
 
-  latexLabel->DrawLatex (x, y, Form("CMS Preliminary            #sqrt{s} = 8 TeV #int Ldt = %.1f fb^{-1}", Lumi));
+//  latexLabel->DrawLatex (x, y, Form("CMS Preliminary            #sqrt{s} = 8 TeV #int Ldt = %.1f fb^{-1}", Lumi));
+  latexLabel->DrawLatex (x, y, Form("                           #sqrt{s} = 8 TeV #int Ldt = %.1f fb^{-1}", Lumi));
   latexLabel->DrawLatex (x, y - 0.12, _decaychannel);
 
   return latexLabel;
