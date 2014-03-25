157dd66a76608012cc0d50f700cfd45a837024df
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2014-03-25 09:07:01 +0100

    drop unused options

--- ZbSkim/test/job_gen.sh
+++ ZbSkim/test/job_gen.sh
@@ -9,6 +9,6 @@ cd $CMS_PATH/slc6_amd64_gcc472/cms/cmssw/CMSSW_6_2_7
 eval `scramv1 runtime -sh`
 cd -
 
-(time hadd $opts rootTuple_$LSB_BATCH_JID.root $@ ) > job.log 2>&1
+(time hadd rootTuple_$LSB_BATCH_JID.root $@ ) > job.log 2>&1
 
 exit
