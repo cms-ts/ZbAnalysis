eb32cd2af6b1c90f058421f5a8dd4e963852c82d
Chiara La Licata <chiara.lalicata@ts.infn.it>
2014-08-05 10:55:09 +0200

    1b/2b distributions added

--- ZbSkim/macros/plot_all8.sh
+++ ZbSkim/macros/plot_all8.sh
@@ -5,24 +5,35 @@ export ROOT_HIST=0
 unset PYTHIA8175DATA
 unset G4NEUTRONXS
 
+n=0
+if [ ! -z "$1" ]; then
+  n=$1
+fi
+
 cd $CMS_PATH/slc6_amd64_gcc472/cms/cmssw/CMSSW_6_2_7
 eval `scramv1 runtime -sh`
 cd -
 
-root -l -q -b DataMCComp8.C+\(\"w_first_bjet_pt\",1,0\)
-root -l -q -b DataMCComp8.C+\(\"w_first_bjet_eta\",1,0\)
+root -l -q -b DataMCComp8.C+\(\"w_first_bjet_pt\",1,0,$n\)
+root -l -q -b DataMCComp8.C+\(\"w_first_bjet_eta\",1,0,$n\)
 root -l -q -b DataMCComp8.C+\(\"w_pt_Z\",1,0\)
-root -l -q -b DataMCComp8.C+\(\"w_y_Z\",1,0\)
 root -l -q -b DataMCComp8.C+\(\"w_delta_phi\",1,0\)
 root -l -q -b DataMCComp8.C+\(\"w_Ht\",1,0\)
-root -l -q -b DataMCComp8.C+\(\"w_mass_Zj\",1,0\)
+
+root -l -q -b DataMCComp8.C+\(\"w_DR_bb\",1,0,2\)
+root -l -q -b DataMCComp8.C+\(\"w_DR_Zb_min\",1,0,2\)
+root -l -q -b DataMCComp8.C+\(\"w_DR_Zb_max\",1,0,2\)
+root -l -q -b DataMCComp8.C+\(\"w_A_Zb\",1,0,2\)
+root -l -q -b DataMCComp8.C+\(\"w_bb_mass\",1,0,2\)
+root -l -q -b DataMCComp8.C+\(\"w_first_bjet_pt\",1,0,2\)
+root -l -q -b DataMCComp8.C+\(\"w_delta_phi_2b\",1,0,2\)
+root -l -q -b DataMCComp8.C+\(\"w_first_bjet_eta\",1,0,2\)
+root -l -q -b DataMCComp8.C+\(\"w_Zbb_mass\",1,0,2\)
 
 root -l -q -b DataMCComp8.C+\(\"w_first_bjet_pt\",1,1\)
 root -l -q -b DataMCComp8.C+\(\"w_first_bjet_eta\",1,1\)
 root -l -q -b DataMCComp8.C+\(\"w_pt_Z\",1,1\)
-root -l -q -b DataMCComp8.C+\(\"w_y_Z\",1,1\)
 root -l -q -b DataMCComp8.C+\(\"w_delta_phi\",1,1\)
 root -l -q -b DataMCComp8.C+\(\"w_Ht\",1,1\)
-root -l -q -b DataMCComp8.C+\(\"w_mass_Zj\",1,1\)
 
 exit
