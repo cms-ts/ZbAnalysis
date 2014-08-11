6b4f77e4ad041ebaee8fddcba6d82943850082b1
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2014-08-11 21:54:51 +0200

    add lumi file for v14

---
+++ ZbSkim/macros/LumiInfo_v14.h
@@ -0,0 +1,113 @@
+string version = "v14";
+
+//////////////////// Integrated luminosity in 1/picobarn
+
+double Lumi2012_ele  = 19789.0; // full 2012 electrons luminosity
+double Lumi2012_muon = 19751.0; // full 2012 muons luminosity
+double Lumi2012_ele_muon = 19780.0; // full 2012 electrons+muons luminosity
+
+// single period lumi, same for electrons and muons dataset in 1/picobarn
+
+double Lumi_2012A_22Jan =  889.362;
+double Lumi_2012B_22Jan = 4429.000;
+double Lumi_2012C_22Jan = 7152.000;
+double Lumi_2012D_22Jan = 7318.000;
+
+//////////////////////// DY MadGraph
+
+double Ngen_dy = 30459503;
+double Xsec_dy = 3503.71; // NNLO
+double eXsec_dy = 0;
+
+//////////////////////// DY Sherpa
+
+double Ngen_dy_1 = 126444144 * 43915740.0 / 127014144.0;
+double Xsec_dy_1 = 3503.71; // NNLO
+double eXsec_dy_1 = 0;
+
+//////////////////////// DY Powheg
+
+double Ngen_dy_2_ee = 3297045; // EE
+double Ngen_dy_2_mm = 3283740; // MM
+double Xsec_dy_2 = 5745.25/3.0; // NNLO
+double eXsec_dy_2 = 0;
+
+//////////////////////// DY MadGraph 2
+
+double Ngen_dy_3 = 7551580;
+double Xsec_dy_3 = 76.75; // LO
+double eXsec_dy_3 = 0;
+
+//////////////////////// TTbar
+
+double Ngen_tt = 6923750;
+double Xsec_tt = 225.197; // NLO
+double eXsec_tt = 0.07*Xsec_tt;
+
+//////////////////////// ZZ
+
+double Ngen_zz = 9799908;
+double Xsec_zz = 8.059; // NLO with CTEQ
+double eXsec_zz = 0.15*Xsec_zz; 
+
+//////////////////////// WZ
+
+double Ngen_wz = 10000283;
+double Xsec_wz = 33.21;    // NLO with CTEQ
+double eXsec_wz = 0.15*Xsec_wz; 
+
+//////////////////////// QCD
+
+double Ngen_qcd = 7529312;
+double Xsec_qcd = 3.64E8; // search the NLO !
+double eXsec_qcd = 0.15*Xsec_qcd; 
+
+//////////////////////// WW
+
+double Ngen_ww = 10000431;
+double Xsec_ww = 54.838; // NLO with CTEQ
+double eXsec_ww = 0.15*Xsec_ww; 
+
+//////////////////////// Wj
+
+double Ngen_wj = 57709905;
+double Xsec_wj = 36703.2; // NNLO
+double eXsec_wj = 0.15*Xsec_wj; 
+
+//////////////////////// single top s-channel
+
+double Ngen_tS = 139974;
+double Xsec_tS = 1.76; // NNO
+double eXsec_tS = 0.15*Xsec_tS; 
+
+//////////////////////// single top t-channel
+
+double Ngen_tT = 1935072;
+double Xsec_tT = 30.7; // NNLO
+double eXsec_tT = 0.15*Xsec_tT; 
+
+//////////////////////// single top tW-channel
+
+double Ngen_tW = 493460;
+double Xsec_tW = 11.1; // NNLO
+double eXsec_tW = 0.15*Xsec_tW; 
+
+//////////////////////// single antitop s-channel
+
+double Ngen_tSb = 259961;
+double Xsec_tSb = 3.79; // NNO
+double eXsec_tSb = 0.15*Xsec_tSb; 
+
+//////////////////////// single antitop t-channel
+
+double Ngen_tTb = 3758227;
+double Xsec_tTb = 56.4; // NNLO
+double eXsec_tTb = 0.15*Xsec_tTb; 
+
+//////////////////////// single antitop tW-channel
+
+double Ngen_tWb = 497658;
+double Xsec_tWb = 11.1; // NNLO
+double eXsec_tWb = 0.15*Xsec_tWb; 
+
+
