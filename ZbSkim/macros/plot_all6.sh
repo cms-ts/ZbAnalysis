ad5ebd6045d9b47161e319f58c69159683e76dcd
Chiara La Licata <chiara.lalicata@ts.infn.it>
2014-08-26 17:15:05 +0200

    modified to run 1b/2b analysis

--- ZbSkim/macros/plot_all6.sh
+++ ZbSkim/macros/plot_all6.sh
@@ -5,6 +5,11 @@ if [ ! -z "$1" ]; then
   d=$1
 fi
 
+n=0
+if [ ! -z "$2" ]; then
+  n=$2
+fi
+
 export ROOT_HIST=0
 
 unset PYTHIA8175DATA
@@ -17,17 +22,25 @@ cd -
 root -l -q -b DataMCComp6.C+\($d,\"w_first_bjet_pt\",1,0\)
 root -l -q -b DataMCComp6.C+\($d,\"w_first_bjet_eta\",1,0\)
 root -l -q -b DataMCComp6.C+\($d,\"w_pt_Z\",1,0\)
-root -l -q -b DataMCComp6.C+\($d,\"w_y_Z\",1,0\)
 root -l -q -b DataMCComp6.C+\($d,\"w_delta_phi\",1,0\)
 root -l -q -b DataMCComp6.C+\($d,\"w_Ht\",1,0\)
 root -l -q -b DataMCComp6.C+\($d,\"w_mass_Zj\",1,0\)
+root -l -q -b DataMCComp6.C+\($d,\"w_Phi_star_ee_b\",1,0,$n\)
+root -l -q -b DataMCComp6.C+\($d,\"w_Phi_star_mm_b\",1,0,$n\)
 
 root -l -q -b DataMCComp6.C+\($d,\"w_first_bjet_pt\",1,1\)
 root -l -q -b DataMCComp6.C+\($d,\"w_first_bjet_eta\",1,1\)
 root -l -q -b DataMCComp6.C+\($d,\"w_pt_Z\",1,1\)
-root -l -q -b DataMCComp6.C+\($d,\"w_y_Z\",1,1\)
 root -l -q -b DataMCComp6.C+\($d,\"w_delta_phi\",1,1\)
 root -l -q -b DataMCComp6.C+\($d,\"w_Ht\",1,1\)
 root -l -q -b DataMCComp6.C+\($d,\"w_mass_Zj\",1,1\)
 
+if [ $n -eq 2 ]; then
+  root -l -q -b DataMCComp6.C+\($d,\"w_DR_bb\",1,0,$n\)
+  root -l -q -b DataMCComp6.C+\($d,\"w_bb_mass\",1,0,$n\)
+  root -l -q -b DataMCComp6.C+\($d,\"w_Ht_b\",1,0,$n\)
+  root -l -q -b DataMCComp6.C+\($d,\"w_delta_phi_2b\",1,0,$n\)
+  root -l -q -b DataMCComp6.C+\($d,\"w_first_bjet_pt\",1,0,$n\)
+  root -l -q -b DataMCComp6.C+\($d,\"w_first_bjet_eta\",1,0,$n\)
+fi
 exit
