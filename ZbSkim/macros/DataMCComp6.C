a848b3faeac072530dd3a61689998c9bdd8d4c3d
Chiara La Licata <chiara.lalicata@ts.infn.it>
2014-08-26 17:13:20 +0200

    modified to run 1b/2b analysis

--- ZbSkim/macros/DataMCComp6.C
+++ ZbSkim/macros/DataMCComp6.C
@@ -2,11 +2,13 @@
 #include "LumiLabel.C"
 #include "LumiInfo_v14.h"
 
-string path = "/gpfs/cms/users/candelis/work/ZbSkim/test/data/";
-//string path = "/gpfs/cms/users/lalicata/work/test/data";
+//string path = "/gpfs/cms/users/candelis/work/ZbSkim/test/data/";
+string path = "/gpfs/cms/users/lalicata/work/test/data/";
 
-void DataMCComp6(int irun=0, string title="", int plot=0, int isratio=1) {
+void DataMCComp6(int irun=0, string title="", int plot=0, int isratio=1, int numB=0) {
 
+string bSel="";
+string dirbSel="";
 string subdir="0";
 string postfix="";
 if (irun==1) {             // irun==1 => JEC Up
@@ -77,29 +79,42 @@ if (irun==99) {            // irun==99 => pur
   subdir="99";
   postfix="Pur";
 }
+if (numB==1) {
+  postfix = postfix + "1b";
+  dirbSel="_1b";
+  bSel="Z + (= 1) b-jet";
+}
+if (numB==2) {
+  postfix = postfix + "2b";
+  dirbSel="_2b";
+  bSel="Z + (#geq 2) b-jet";
+}
 
         string title_ee = title;
         string title_ee_b = title;
         string title_mm = title;
         string title_mm_b = title;
 
-        if (title=="w_pt_Z"||title=="w_mass_Zj"||title=="w_delta_phi"||title=="w_y_Z") {
-          title_ee = title_ee + "_ee";
-          title_ee_b = title_ee_b + "_ee";
-          title_mm = title_mm + "_mm";
-          title_mm_b = title_mm_b + "_mm";
+
+        if (numB==0) {
+          if (title=="w_pt_Z"||title=="w_mass_Zj"||title=="w_delta_phi") {
+            title_ee = title_ee + "_ee";
+            title_ee_b = title_ee_b + "_ee";
+            title_mm = title_mm + "_mm";
+            title_mm_b = title_mm_b + "_mm";
+          }
+
+          if (title.find("_bjet_")!=string::npos) {
+            title_ee.erase(title_ee.find("_bjet_")+1, 1);
+            title_mm.erase(title_mm.find("_bjet_")+1, 1);
+          } else {
+            title_ee_b = title_ee + "_b";
+            title_mm_b = title_mm + "_b";
+          }
         }
 
-        if (title.find("_bjet_")!=string::npos) {
-          title_ee.erase(title_ee.find("_bjet_")+1, 1);
-          title_mm.erase(title_mm.find("_bjet_")+1, 1);
-        } else {
-          title_ee_b = title_ee + "_b";
-          title_mm_b = title_mm + "_b";
-        }
-
-        TFile f_ee((path + "/electrons/" + version + "/" + subdir + "/unfolding/" + title_ee + "_unfolding.root").c_str());
-        TFile f_ee_b((path + "/electrons/" + version + "/" + subdir + "/unfolding/" + title_ee_b + "_unfolding.root").c_str());
+        TFile f_ee((path + "/electrons/" + version + "/" + subdir + "/unfolding" + dirbSel + "/" + title_ee + "_unfolding.root").c_str());
+        TFile f_ee_b((path + "/electrons/" + version + "/" + subdir + "/unfolding" + dirbSel + "/" + title_ee_b + "_unfolding.root").c_str());
         TH1F* h_data_ee = (TH1F*)f_ee.Get(title_ee.c_str())->Clone();
         TH1F* h_data_ee_b = (TH1F*)f_ee_b.Get(title_ee_b.c_str())->Clone();
         h_data_ee->SetDirectory(0);
@@ -107,14 +122,19 @@ if (irun==99) {            // irun==99 => pur
         f_ee.Close();
         f_ee_b.Close();
 
-        TFile f_mm((path + "/muons/" + version + "/" + subdir + "/unfolding/" + title_mm + "_unfolding.root").c_str());
-        TFile f_mm_b((path + "/muons/" + version + "/" + subdir + "/unfolding/" + title_mm_b + "_unfolding.root").c_str());
+        TFile f_mm((path + "/muons/" + version + "/" + subdir + "/unfolding" + dirbSel + "/" + title_mm + "_unfolding.root").c_str());
+        TFile f_mm_b((path + "/muons/" + version + "/" + subdir + "/unfolding" + dirbSel + "/" + title_mm_b + "_unfolding.root").c_str());
         TH1F* h_data_mm = (TH1F*)f_mm.Get(title_mm.c_str())->Clone();
         TH1F* h_data_mm_b = (TH1F*)f_mm_b.Get(title_mm_b.c_str())->Clone();
         h_data_mm->SetDirectory(0);
         h_data_mm_b->SetDirectory(0);
         f_mm.Close();
         f_mm_b.Close();
+
+        if (numB!=0) {
+          h_data_ee = (TH1F*)h_data_ee->Clone();
+          h_data_mm = (TH1F*)h_data_mm->Clone();
+        }
 
         h_data_ee->SetStats(0);
         h_data_ee_b->SetStats(0);
@@ -246,12 +266,12 @@ if (irun==99) {            // irun==99 => pur
 
 	if (plot) {
           if (isratio==0) {
-            gSystem->mkdir((path + "/combined/" + version + "/" + subdir + "/xsecs_unfolding/").c_str(), kTRUE);
-            c1->SaveAs((path + "/combined/" + version + "/" + subdir + "/xsecs_unfolding/" + title + "_xsecs_unfolding.pdf").c_str());
+            gSystem->mkdir((path + "/combined/" + version + "/" + subdir + "/xsecs_unfolding" + dirbSel + "/").c_str(), kTRUE);
+            c1->SaveAs((path + "/combined/" + version + "/" + subdir + "/xsecs_unfolding" + dirbSel + "/" + title + "_xsecs_unfolding.pdf").c_str());
           }
           if (isratio==1) {
-            gSystem->mkdir((path + "/combined/" + version + "/" + subdir + "/ratios_unfolding/").c_str(), kTRUE);
-            c1->SaveAs((path + "/combined/" + version + "/" + subdir + "/ratios_unfolding/" + title + "_ratios_unfolding.pdf").c_str());
+            gSystem->mkdir((path + "/combined/" + version + "/" + subdir + "/ratios_unfolding" + dirbSel + "/").c_str(), kTRUE);
+            c1->SaveAs((path + "/combined/" + version + "/" + subdir + "/ratios_unfolding" + dirbSel + "/" + title + "_ratios_unfolding.pdf").c_str());
           }
 	}
 
