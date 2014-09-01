9e0d7e207c7f7344f284eb11b0b0946eba91daee
Chiara La Licata <chiara.lalicata@ts.infn.it>
2014-09-01 14:30:10 +0200

    1b/2b distributions added

--- ZbSkim/macros/plot_all7.sh
+++ ZbSkim/macros/plot_all7.sh
@@ -19,11 +19,17 @@ while [ $i -le 2 ]; do
 
   root -l -q -b DataMCComp7.C+\(\"w_first_bjet_pt\",1,$i,0,$n\)
   root -l -q -b DataMCComp7.C+\(\"w_first_bjet_eta\",1,$i,0,$n\)
-  root -l -q -b DataMCComp7.C+\(\"w_pt_Z_ee\",1,$i,0,$n\)
-  root -l -q -b DataMCComp7.C+\(\"w_pt_Z_mm\",1,$i,0,$n\)
-  root -l -q -b DataMCComp7.C+\(\"w_Ht\",1,$i,0,$n\)
-  root -l -q -b DataMCComp7.C+\(\"w_delta_phi_ee\",1,$i,0,$n\)
-  root -l -q -b DataMCComp7.C+\(\"w_delta_phi_mm\",1,$i,0,$n\)
+  root -l -q -b DataMCComp7.C+\(\"w_pt_Z_ee\",1,$i,0\)
+  root -l -q -b DataMCComp7.C+\(\"w_pt_Z_mm\",1,$i,0\)
+  root -l -q -b DataMCComp7.C+\(\"w_Ht\",1,$i,0\)
+  root -l -q -b DataMCComp7.C+\(\"w_delta_phi_ee\",1,$i,0\)
+  root -l -q -b DataMCComp7.C+\(\"w_delta_phi_mm\",1,$i,0\)
+
+  if [ $n -ne 0 ]; then
+    root -l -q -b DataMCComp7.C+\(\"w_pt_Z_ee_b\",1,$i,0,$n\)
+    root -l -q -b DataMCComp7.C+\(\"w_pt_Z_mm_b\",1,$i,0,$n\)
+    root -l -q -b DataMCComp7.C+\(\"w_Ht_b\",1,$i,0,$n\)
+  fi
 
   root -l -q -b DataMCComp7.C+\(\"w_DR_bb\",1,$i,0,2\)
   root -l -q -b DataMCComp7.C+\(\"w_bb_mass\",1,$i,0,2\)
