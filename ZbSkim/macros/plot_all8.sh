cd54cccc07bbe09a75eb2188f0bc35409e62f8a1
Chiara La Licata <chiara.lalicata@ts.infn.it>
2014-09-01 14:30:45 +0200

    1b/2b distributions added

--- ZbSkim/macros/plot_all8.sh
+++ ZbSkim/macros/plot_all8.sh
@@ -20,6 +20,11 @@ root -l -q -b DataMCComp8.C+\(\"w_pt_Z\",1,0\)
 root -l -q -b DataMCComp8.C+\(\"w_delta_phi\",1,0\)
 root -l -q -b DataMCComp8.C+\(\"w_Ht\",1,0\)
 
+if [ $n -ne 0 ]; then
+  root -l -q -b DataMCComp8.C+\(\"w_pt_Z_b\",1,0,$n\)
+  root -l -q -b DataMCComp8.C+\(\"w_Ht_b\",1,0,$n\)
+fi
+
 root -l -q -b DataMCComp8.C+\(\"w_DR_bb\",1,0,2\)
 root -l -q -b DataMCComp8.C+\(\"w_DR_Zb_min\",1,0,2\)
 root -l -q -b DataMCComp8.C+\(\"w_DR_Zb_max\",1,0,2\)
