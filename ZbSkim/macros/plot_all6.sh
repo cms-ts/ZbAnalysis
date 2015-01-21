77e468fb690c83a1e64c720d5682b6fbb5fddf98
Chiara La Licata <chiara.lalicata@ts.infn.it>
2015-01-21 15:03:05 +0100

    unused plots removed and w_first_bjet_eta_abs added

--- ZbSkim/macros/plot_all6.sh
+++ ZbSkim/macros/plot_all6.sh
@@ -19,17 +19,17 @@ cd $CMS_PATH/slc6_amd64_gcc472/cms/cmssw/CMSSW_6_2_7
 eval `scramv1 runtime -sh`
 cd -
 
-root -l -q -b DataMCComp6.C+\($d,\"w_first_bjet_pt\",1,0\)
-root -l -q -b DataMCComp6.C+\($d,\"w_first_bjet_eta\",1,0\)
-root -l -q -b DataMCComp6.C+\($d,\"w_pt_Z\",1,0\)
-root -l -q -b DataMCComp6.C+\($d,\"w_delta_phi\",1,0\)
-root -l -q -b DataMCComp6.C+\($d,\"w_Ht\",1,0\)
+root -l -q -b DataMCComp6.C+\($d,\"w_first_bjet_pt\",1,0,$n\)
+root -l -q -b DataMCComp6.C+\($d,\"w_first_bjet_eta\",1,0,$n\)
+root -l -q -b DataMCComp6.C+\($d,\"w_first_bjet_eta_abs\",1,0\)
+root -l -q -b DataMCComp6.C+\($d,\"w_pt_Z\",1,0,$n\)
+root -l -q -b DataMCComp6.C+\($d,\"w_delta_phi\",1,0,$n\)
+root -l -q -b DataMCComp6.C+\($d,\"w_Ht\",1,0,$n\)
 root -l -q -b DataMCComp6.C+\($d,\"w_mass_Zj\",1,0\)
-root -l -q -b DataMCComp6.C+\($d,\"w_Phi_star_ee_b\",1,0,$n\)
-root -l -q -b DataMCComp6.C+\($d,\"w_Phi_star_mm_b\",1,0,$n\)
 
 root -l -q -b DataMCComp6.C+\($d,\"w_first_bjet_pt\",1,1\)
 root -l -q -b DataMCComp6.C+\($d,\"w_first_bjet_eta\",1,1\)
+root -l -q -b DataMCComp6.C+\($d,\"w_first_bjet_eta_abs\",1,1\)
 root -l -q -b DataMCComp6.C+\($d,\"w_pt_Z\",1,1\)
 root -l -q -b DataMCComp6.C+\($d,\"w_delta_phi\",1,1\)
 root -l -q -b DataMCComp6.C+\($d,\"w_Ht\",1,1\)
