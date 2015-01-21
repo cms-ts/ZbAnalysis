6de6acda7a7f0b0e67e8f3805f4054641c7bb725
Chiara La Licata <chiara.lalicata@ts.infn.it>
2015-01-21 15:13:03 +0100

    modified to produce some plots only in the 1b/2b cases and w_first_bjet_eta_abs added

--- ZbSkim/macros/plot_all7.sh
+++ ZbSkim/macros/plot_all7.sh
@@ -19,6 +19,7 @@ while [ $i -le 2 ]; do
 
   root -l -q -b DataMCComp7.C+\(\"w_first_bjet_pt\",1,$i,0,$n\)
   root -l -q -b DataMCComp7.C+\(\"w_first_bjet_eta\",1,$i,0,$n\)
+  root -l -q -b DataMCComp7.C+\(\"w_first_bjet_eta_abs\",1,$i,0,$n\)
   root -l -q -b DataMCComp7.C+\(\"w_pt_Z_ee\",1,$i,0\)
   root -l -q -b DataMCComp7.C+\(\"w_pt_Z_mm\",1,$i,0\)
   root -l -q -b DataMCComp7.C+\(\"w_Ht\",1,$i,0\)
@@ -29,21 +30,27 @@ while [ $i -le 2 ]; do
     root -l -q -b DataMCComp7.C+\(\"w_pt_Z_ee_b\",1,$i,0,$n\)
     root -l -q -b DataMCComp7.C+\(\"w_pt_Z_mm_b\",1,$i,0,$n\)
     root -l -q -b DataMCComp7.C+\(\"w_Ht_b\",1,$i,0,$n\)
+    root -l -q -b DataMCComp7.C+\(\"w_mass_Zj_ee_b\",1,$i,0,$n\)
+    root -l -q -b DataMCComp7.C+\(\"w_mass_Zj_mm_b\",1,$i,0,$n\)
+    root -l -q -b DataMCComp7.C+\(\"w_delta_phi_ee_b\",1,$i,0,$n\)
+    root -l -q -b DataMCComp7.C+\(\"w_delta_phi_mm_b\",1,$i,0,$n\)
   fi
 
-  root -l -q -b DataMCComp7.C+\(\"w_DR_bb\",1,$i,0,2\)
-  root -l -q -b DataMCComp7.C+\(\"w_bb_mass\",1,$i,0,2\)
-  root -l -q -b DataMCComp7.C+\(\"w_first_bjet_pt\",1,$i,0,2\)
-  root -l -q -b DataMCComp7.C+\(\"w_eebb_mass\",1,$i,0,2\)
-  root -l -q -b DataMCComp7.C+\(\"w_mmbb_mass\",1,$i,0,2\)
-  root -l -q -b DataMCComp7.C+\(\"w_delta_phi_2b\",1,$i,0,2\)
-  root -l -q -b DataMCComp7.C+\(\"w_first_bjet_eta\",1,$i,0,2\)
-  root -l -q -b DataMCComp7.C+\(\"w_DR_eeb_min\",1,$i,0,2\)
-  root -l -q -b DataMCComp7.C+\(\"w_DR_eeb_max\",1,$i,0,2\)
-  root -l -q -b DataMCComp7.C+\(\"w_DR_mmb_min\",1,$i,0,2\)
-  root -l -q -b DataMCComp7.C+\(\"w_DR_mmb_max\",1,$i,0,2\)
-  root -l -q -b DataMCComp7.C+\(\"w_A_eeb\",1,$i,0,2\)
-  root -l -q -b DataMCComp7.C+\(\"w_A_mmb\",1,$i,0,2\)
+  if [ $n -eq 2 ]; then
+    root -l -q -b DataMCComp7.C+\(\"w_DR_bb\",1,$i,0,2\)
+    root -l -q -b DataMCComp7.C+\(\"w_bb_mass\",1,$i,0,2\)
+    root -l -q -b DataMCComp7.C+\(\"w_first_bjet_pt\",1,$i,0,2\)
+    root -l -q -b DataMCComp7.C+\(\"w_eebb_mass\",1,$i,0,2\)
+    root -l -q -b DataMCComp7.C+\(\"w_mmbb_mass\",1,$i,0,2\)
+    root -l -q -b DataMCComp7.C+\(\"w_delta_phi_2b\",1,$i,0,2\)
+    root -l -q -b DataMCComp7.C+\(\"w_first_bjet_eta\",1,$i,0,2\)
+    root -l -q -b DataMCComp7.C+\(\"w_DR_eeb_min\",1,$i,0,2\)
+    root -l -q -b DataMCComp7.C+\(\"w_DR_eeb_max\",1,$i,0,2\)
+    root -l -q -b DataMCComp7.C+\(\"w_DR_mmb_min\",1,$i,0,2\)
+    root -l -q -b DataMCComp7.C+\(\"w_DR_mmb_max\",1,$i,0,2\)
+    root -l -q -b DataMCComp7.C+\(\"w_A_eeb\",1,$i,0,2\)
+    root -l -q -b DataMCComp7.C+\(\"w_A_mmb\",1,$i,0,2\)
+  fi
 
   i=$((i+1))
 done
@@ -53,6 +60,7 @@ while [ $i -le 2 ]; do
 
   root -l -q -b DataMCComp7.C+\(\"w_first_bjet_pt\",1,$i,1\)
   root -l -q -b DataMCComp7.C+\(\"w_first_bjet_eta\",1,$i,1\)
+  root -l -q -b DataMCComp7.C+\(\"w_first_bjet_eta_abs\",1,$i,1\)
   root -l -q -b DataMCComp7.C+\(\"w_pt_Z_ee\",1,$i,1\)
   root -l -q -b DataMCComp7.C+\(\"w_pt_Z_mm\",1,$i,1\)
   root -l -q -b DataMCComp7.C+\(\"w_Ht\",1,$i,1\)
