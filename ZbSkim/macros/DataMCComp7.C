99666356f40f21f8c582e7677bcdf9777b44e1f9
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2015-08-02 12:55:13 +0200

    fix typos

--- ZbSkim/macros/DataMCComp7.C
+++ ZbSkim/macros/DataMCComp7.C
@@ -1687,7 +1687,7 @@ if (numB==2) {
 	  }
 	} else if (title_b=="w_mass_Zj_ee_b" || title_b=="w_mass_Zj_mm_b") {
 	  h_mc1b_b->GetYaxis()->SetTitle("d#sigma / dM_{Zj} [pb]");
-	  h_P_tot->GetXaxis()->SetTitle("M(Zj) [GeV^{2}]");
+	  h_P_tot->GetXaxis()->SetTitle("M(Zj) [GeV/c^{2}]");
 	  if (isratio==1) {
 	    h_mc1b_b->GetYaxis()->SetTitle("d[#sigma(Z+b) / #sigma(Z+j)] / dM_{Zj} [%]");
 	    h_mc1b_b->GetYaxis()->SetRangeUser(0, 20);
--- ZbSkim/macros/DataMCComp8.C
+++ ZbSkim/macros/DataMCComp8.C
@@ -2697,7 +2697,7 @@ if (numB==2) {
 	  }
 	} else if (title_b=="w_mass_Zj_b") {
 	  h_mcg_b->GetYaxis()->SetTitle("d#sigma / dM_{Zj} [pb]");
-	  h_P_tot->GetXaxis()->SetTitle("M(Zj) [GeV^{2}]");
+	  h_P_tot->GetXaxis()->SetTitle("M(Zj) [GeV/c^{2}]");
 	  if (isratio==1) {
 	    h_mcg_b->GetYaxis()->SetTitle("d[#sigma(Z+b) / #sigma(Z+j)] / dM_{Zj} [%]");
 	    h_mcg_b->GetYaxis()->SetRangeUser(0, 20);
